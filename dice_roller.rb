8c7b340e004f739c7070873fbe8fd64795327f69
JCembala <cembala.jakub@gmail.com>
2022-05-17 13:20:48 +0200

    task 2 done

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,17 +1,33 @@
-# Please write a script called dice_roller.rb. 
-# When launched using shell (ruby dice_roller.rb) it should display a randomly generated number from 1-20 range.
-# Imagine this as rolling a twenty sided dice (k20).
+# In this iteration, running the dice_roller.rb should return 5 randomly generated numbers (five dice rolls).
+# However, the script should obtain the set by initially generating 7 dice rolls and excluding the highest and the lowest ones!
+# It is extremely important not to change the order of the numbers.
 
 class Dice
 
   def initialize(size = 20)
     @size = size
+    @set = []
   end
 
-  def roll
-    rand(1..@size)
+  def roll_dice(dice_size = @size)
+    rand(1..dice_size)
+  end
+
+  def roll_set
+    clear_set
+    7.times { @set << roll_dice }
+    @set.delete_at(@set.index(@set.max))
+    @set.delete_at(@set.index(@set.min))
+
+    @set
+  end
+
+  def clear_set
+    @set = []
   end
 end
 
 dice = Dice.new
-p dice.roll
+rolls = dice.roll_set
+
+p rolls
