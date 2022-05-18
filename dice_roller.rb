6146c91a6a7f23cbd36e9751e068cc8bb1aa7ee9
JCembala <cembala.jakub@gmail.com>
2022-05-18 08:51:58 +0200

    task 5 done

--- dice_roller.rb
+++ dice_roller.rb
@@ -1,5 +1,6 @@
-# In this iteration we would like to perform additional roll, which would be appended to the set as last.
-# Again, this should not count for the at least 55 rule.
+# Before performing the roll specified in the point 2) generate a random number from within 1 - 30 range (i.e., roll a k30).
+# If the result is contained within the 1-7 range (included), perform another k30 roll.
+# Then if the result of additional roll is 1, write a logic which changes the behavior of throwing away the highest and the lowest numbers mentioned in the point 2) to: exclude two lowest numbers from the set.
 class Dice
 
   def initialize(size = 20)
@@ -7,21 +8,31 @@ class Dice
     @set = []
   end
 
+  def roll
+    roll_set_with_at_least_55_points
+    roll_additional_dice
+  end
+
+  private
+
   def roll_dice(dice_size = @size)
     rand(1..dice_size)
   end
 
   def roll_set
     clear_set
+    k30_set = []
+    k30_set << roll_dice(30)
+    k30_set << roll_dice(30) if k30_set.first.between?(1, 7)
+
     7.times { @set << roll_dice }
-    @set.delete_at(@set.index(@set.max))
-    @set.delete_at(@set.index(@set.min))
 
-    @set
-  end
-
-  def clear_set
-    @set = []
+    if k30_set[1] == 1
+      2.times { @set.delete_at(@set.index(@set.min)) }
+    else
+      @set.delete_at(@set.index(@set.max))
+      @set.delete_at(@set.index(@set.min))
+    end
   end
 
   def roll_set_with_at_least_55_points
@@ -41,7 +52,11 @@ class Dice
     roll_set_with_at_least_55_points
     roll_additional_dice
   end
+
+  def clear_set
+    @set = []
+  end
 end
 
 dice = Dice.new
-p dice.final_roll
+p dice.roll
